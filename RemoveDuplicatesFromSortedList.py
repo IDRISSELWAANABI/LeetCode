a1e41459f990ad58051b8970a6b0504e4ca3efdf
IDRISSELWAANABI <drisselwaanabi02@gmail.com>
2024-01-27 09:46:10 +0100

    new problem solved

---
+++ RemoveDuplicatesFromSortedList.py
@@ -0,0 +1,18 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head  
+        
+        current = head
+        while current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return head
+
+        
