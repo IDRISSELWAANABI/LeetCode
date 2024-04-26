6c5d1c1fa5f1e4b97f166654ceebc11a7df429d0
IDRISSELWAANABI <drisselwaanabi02@gmail.com>
2024-04-26 11:24:56 +0100

    corrected

--- TwoSum_II.py
+++ TwoSum_II.py
@@ -1,6 +1,5 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        res = []
         l,r = 0,len(numbers)-1
         while l<r : 
             if numbers[l]+numbers[r]<target : 
