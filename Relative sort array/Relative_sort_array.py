85ff30ed30f2790edab422a5bdd0e47d0a4c7e99
IDRISSELWAANABI <drisselwaanabi02@gmail.com>
2024-06-11 18:39:01 +0100

    second try

--- Relative sort array/Relative_sort_array.py
+++ Relative sort array/Relative_sort_array.py
@@ -1,24 +1,13 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        n = len(arr1)
-        m = len(arr2)
         res = []
-        s = set()
-
-
-        for i in range(m):
-            for j in range(n):
-                if arr1[j]==arr2[i]:
-                    res.append(arr2[i])
-                    s.add(arr2[i])
-        res1 = []
-        for i in arr1 : 
-            if i not in s : 
-                res1.append(i)
-
-        res = res+sorted(res1)
-                    
-
+        temp = []
+        for i in arr2 :  
+            res.extend([i]*arr1.count(i))
+        for i in arr1 :
+            if  i not in arr2 : 
+                temp.append(i)
+        res.extend(sorted(temp))
         return res 
             
 
