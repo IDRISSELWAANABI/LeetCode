2901a7f36f35fb2cab8e3ace40159405d5acc4f1
IDRISSELWAANABI <drisselwaanabi02@gmail.com>
2024-06-12 15:43:46 +0100

    the most optimized python solution O(n) time complexity and O(1) space complexity

--- Sort Colors/Sort_colors.py
+++ Sort Colors/Sort_colors.py
@@ -1,7 +1,14 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        n = len(nums)
-        for j in range(n):
-            for i in range(n-1): 
-                if nums[i]>nums[i+1]:
-                    nums[i],nums[i+1] = nums[i+1], nums[i]
+        low, mid, high = 0, 0, len(nums) - 1
+        
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high -= 1
