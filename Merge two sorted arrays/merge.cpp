85b7dacd5ea6a571a26660f7243f3f80148392d2
IDRISSELWAANABI <drisselwaanabi02@gmail.com>
2024-06-17 00:51:58 +0100

    adding cpp solution

---
+++ Merge two sorted arrays/merge.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int last1 = m-1 ;
+        int last2 = n-1 ; 
+        int last = n+m-1;
+        while (last1 >= 0 && last2 >= 0){
+            if(nums1[last1]>nums2[last2]){
+                nums1[last]=nums1[last1];
+                last1--;
+            }else{
+                nums1[last]=nums2[last2];
+                last2--;
+            }
+            last--;
+        }
+        while(last2 >= 0){
+            nums1[last] = nums2[last2];
+            last--;
+            last2--;
+        }
+        
+    }
+};
