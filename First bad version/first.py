e8a8486450c22f46f0b0cf3bb0f7fad319104bc4
IDRISSELWAANABI <drisselwaanabi02@gmail.com>
2024-06-19 12:27:08 +0100

    problem solved using binary search

---
+++ First bad version/first.py
@@ -0,0 +1,16 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        low = 1
+        high = n
+        while low < high : 
+            mid = (low+high)//2
+            if isBadVersion(mid):
+                high = mid
+            else : 
+                low = mid+1
+        return low 
+
+        
