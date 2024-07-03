078c5df3cd414db81654f7da44b1e6d1b6094bb0
IDRISSELWAANABI <drisselwaanabi02@gmail.com>
2024-07-03 23:19:02 +0100

    adding java solution

---
+++ Intersection of two arrays/intersection.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        List<Integer> result = new ArrayList<>();
+        int i = 0, j = 0;
+        
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                result.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
